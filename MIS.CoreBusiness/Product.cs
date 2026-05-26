fd721e356edf07f2303ecc9e0462e5e3d5d45419
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-05-26 19:39:52 +0530

    Product Inventories - Many to Many Relation

--- MIS.CoreBusiness/Product.cs
+++ MIS.CoreBusiness/Product.cs
@@ -12,5 +12,30 @@ namespace MIS.CoreBusiness
         public int Quantity { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal than 0.")]
         public double Price { get; set; }
+
+        // Many to Many
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+        // Helper function to add product inventories
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any(x => x.Inventory is not null && x.Inventory.InventoryName.Equals(inventory.InventoryName)))
+            {
+                this.ProductInventories.Add(
+                        new ProductInventory
+                        {
+                            InventoryId = inventory.InventoryId,
+                            ProductId = this.ProductId,
+                            Inventory = inventory,
+                            Product = this,
+                        }
+                    );
+            }
+        }
+
+        public void RemoveInventory(ProductInventory prodInv)
+        {
+            this.ProductInventories.Remove(prodInv);
+        }
     }
 }
---
+++ MIS.CoreBusiness/ProductInventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MIS.CoreBusiness
+{
+    // Many to many relationship between product and inventory.
+    public class ProductInventory
+    {
+        public int ProductId { get; set; }
+        public int InventoryId { get; set; }
+        public int InventoryQuantity { get; set; }
+
+        // Navigation property
+        [JsonIgnore]
+        public Product? Product { get; set; }
+        [JsonIgnore]
+        public Inventory? Inventory { get; set; }
+    }
+}
